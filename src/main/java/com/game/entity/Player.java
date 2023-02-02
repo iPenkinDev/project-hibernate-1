ba723fe0b6e25c34dc115aad57b75669b5d1dd39
Igor Penkin <igor.penkin.dev@gmail.com>
2023-02-02 16:17:18 +0300

    ready

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,42 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
+
 import java.util.Date;
 
-
+@Entity
+@Table(schema = "rpg", name = "player")
+@NamedQuery(name = "getAllCount", query = "select count(p) from Player p")
 public class Player {
+    @Id
+    @NonNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(length = 12)
+    @NonNull
     private String name;
 
+    @Column(length = 30)
+    @NonNull
     private String title;
 
+    @Enumerated(EnumType.ORDINAL)
+    @NonNull
     private Race race;
 
+    @Enumerated(EnumType.ORDINAL)
+    @NonNull
     private Profession profession;
 
+    @NonNull
     private Date birthday;
 
+    @NonNull
     private Boolean banned;
 
+    @NonNull
     private Integer level;
 
     public Player() {
